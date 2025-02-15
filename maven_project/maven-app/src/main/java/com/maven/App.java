8d6de84ab9b664de121add5951b28a108a99c667
Meghana B <meghanab235@gmail.com>
2025-02-15 18:06:22 +0530

    maven-app
    maven

--- maven_project/maven-app/src/main/java/com/maven/App.java
+++ maven_project/maven-app/src/main/java/com/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello welecome to my maven project" );
+        System.out.println( "Hello welecome to my maven project in my repo" );
     }
 }
